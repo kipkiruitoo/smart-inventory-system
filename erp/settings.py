86ce62ccd1f1046067c3cafc2eb3a86857a9ac54
gkarumba <gachegua@gmail.com>
2019-11-06 10:55:05 +0300

    Make the intial migration

--- erp/settings.py
+++ erp/settings.py
@@ -124,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'Africa/Nairobi'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
